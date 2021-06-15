c78557d4a41496288800fe586d5ddf481a515f1e
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-06-15 15:40:27 +0700

    # 53 using Dynamic programming method, including test cases - Remove unused import

--- src/com/minhle/practiceleetcode/MaximumSubarray.java
+++ src/com/minhle/practiceleetcode/MaximumSubarray.java
@@ -2,8 +2,6 @@ package com.minhle.practiceleetcode;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-import static java.lang.Math.*;
-
 public class MaximumSubarray {
     public int maxSubArray(int[] nums) {
         int result = nums[0];
