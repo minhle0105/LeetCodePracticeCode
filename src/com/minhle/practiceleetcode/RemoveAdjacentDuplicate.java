e657dfbef2e23ff62164dd2ae03f6300af4c760c
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-06-29 17:23:34 +0700

    #1047 with test cases - needs to improve efficiency

---
+++ src/com/minhle/practiceleetcode/RemoveAdjacentDuplicate.java
@@ -0,0 +1,33 @@
+package com.minhle.practiceleetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class RemoveAdjacentDuplicate {
+    public String removeDuplicates(String s) {
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i+1)) {
+                String duplicateCharacters = String.valueOf(s.charAt(i));
+                duplicateCharacters += String.valueOf(s.charAt(i+1));
+                s = s.replaceFirst(duplicateCharacters, "");
+                return removeDuplicates(s);
+            }
+        }
+        return s;
+    }
+
+    @Test
+    void firstTest() {
+        String expected = "ca";
+        String actual = removeDuplicates("abbaca");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void secondTest() {
+        String expected = "ay";
+        String actual = removeDuplicates("azxxzy");
+        Assertions.assertEquals(expected, actual);
+    }
+}
