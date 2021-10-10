b9e382545978c468d5f132935080fa41d11e2789
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-10-09 22:22:38 -0400

    Add test cases and helper sort function for #349 and #350, because array is returned in any order, we need to sort expected and actual to test

--- src/com/minhle/practiceleetcode/IntersectionOfTwoArraysII.java
+++ src/com/minhle/practiceleetcode/IntersectionOfTwoArraysII.java
@@ -3,6 +3,9 @@ package com.minhle.practiceleetcode;
 import java.util.Arrays;
 import java.util.HashMap;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class IntersectionOfTwoArraysII {
 
     private HashMap<Integer, Integer> getFrequency(int[] nums) {
@@ -38,6 +41,33 @@ public class IntersectionOfTwoArraysII {
         }
         result = Arrays.copyOfRange(result, 0, counter);
         return result;
+    }
+
+    void bubbleSort(int[] arr) {
+        int n = arr.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (arr[j - 1] > arr[j]) {
+                    //swap elements
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+
+            }
+        }
+    }
+
+    @Test
+    void firstTest(){
+        int[] nums1 = new int[]{61,24,20,58,95,53,17,32,45,85,70,20,83,62,35,89,5,95,12,86,58,77,30,64,46,13,5,92,67,40,20,38,31,18,89,85,7,30,67,34,62,35,47,98,3,41,53,26,66,40,54,44,57,46,70,60,4,63,82,42,65,59,17,98,29,72,1,96,82,66,98,6,92,31,43,81,88,60,10,55,66,82,0,79,11,81};
+        int[] nums2 = new int[]{5,25,4,39,57,49,93,79,7,8,49,89,2,7,73,88,45,15,34,92,84,38,85,34,16,6,99,0,2,36,68,52,73,50,77,44,61,48}; // 2 -> 2
+        int[] expected = new int[]{5,4,57,79,7,89,88,45,34,92,38,85,6,0,77,44,61};
+        int[] actual = intersect(nums1, nums2);
+        bubbleSort(expected);
+        bubbleSort(actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
 
 }
--- src/com/minhle/practiceleetcode/IntersectionsOfTwoArrays.java
+++ src/com/minhle/practiceleetcode/IntersectionsOfTwoArrays.java
@@ -3,48 +3,72 @@ package com.minhle.practiceleetcode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class IntersectionsOfTwoArrays {
-    public int[] intersection(int[] nums1, int[] nums2) {
+    private HashMap<Integer, Integer> getFrequency(int[] nums) {
         HashMap<Integer, Integer> hashMap = new HashMap<>();
-        for (int i : nums1) {
-            if (!hashMap.containsKey(i)) {
-                hashMap.put(i, 1);
+        for (int i : nums) {
+            int addValue = hashMap.containsKey(i) ? hashMap.get(i) + 1 : 1;
+            hashMap.put(i, addValue);
+        }
+        return hashMap;
+    }
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> hashMap1 = getFrequency(nums1);
+        HashMap<Integer, Integer> hashMap2 = getFrequency(nums2);
+        int l = nums1.length < nums2.length ? nums1.length : nums2.length;
+        int[] result = new int[l];
+        int counter = 0;
+        for (int i : hashMap1.keySet()) {
+            if (hashMap2.containsKey(i)) {
+                result[counter++] = i;
             }
-            else {
-                hashMap.put(i, hashMap.get(i) + 1);
+            if (counter == l) {
+                break;
             }
         }
-        ArrayList<Integer> results = new ArrayList<>();
-        for (int i : nums2) {
-            if (hashMap.containsKey(i) && (!results.contains(i))) {
-                results.add(i);
+        result = Arrays.copyOfRange(result, 0, counter);
+        return result;
+    }
+
+    void bubbleSort(int[] arr) {
+        int n = arr.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (arr[j - 1] > arr[j]) {
+                    //swap elements
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+
             }
         }
-        int[] finalResult = new int[results.size()];
-        for (int i = 0; i < finalResult.length; i++) {
-            finalResult[i] = results.get(i);
-        }
-        return finalResult;
     }
 
     @Test
     void firstTest() {
         int[] expected = {2};
-        int[] nums1 = {1,2,2,1};
-        int[] nums2 = {2,2};
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2};
         int[] actual = intersection(nums1, nums2);
+        bubbleSort(actual);
+        bubbleSort(expected);
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
     void secondTest() {
-        int[] expected = {9,4};
-        int[] nums1 = {4,9,5};
-        int[] nums2 = {9,4,9,8,4};
+        int[] expected = {9, 4};
+        int[] nums1 = {4, 9, 5};
+        int[] nums2 = {9, 4, 9, 8, 4};
         int[] actual = intersection(nums1, nums2);
+        bubbleSort(actual);
+        bubbleSort(expected);
         Assertions.assertArrayEquals(expected, actual);
     }
 
