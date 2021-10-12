9c2b1ef9a11461c8e3a9dc47763a123921091dbf
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-10-12 01:30:33 -0400

    #387 Improved

--- src/com/minhle/practiceleetcode/FirstUniqueCharacterInString.java
+++ src/com/minhle/practiceleetcode/FirstUniqueCharacterInString.java
@@ -1,24 +1,24 @@
 package com.minhle.practiceleetcode;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.HashMap;
 
 public class FirstUniqueCharacterInString {
     public int firstUniqChar(String s) {
-        LinkedHashMap<Character, Integer> hashMap = new LinkedHashMap<>();
+        HashMap<Character, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
-            if (!hashMap.containsKey(s.charAt(i))) {
-                hashMap.put(s.charAt(i), 1);
-            }
-            else {
+            if (hashMap.containsKey(s.charAt(i))) {
                 hashMap.put(s.charAt(i), hashMap.get(s.charAt(i)) + 1);
             }
+            else {
+                hashMap.put(s.charAt(i), 1);
+            }
         }
-        for (Map.Entry mapElement : hashMap.entrySet()) {
-            if ((Integer) mapElement.getValue() == 1) {
-                return s.indexOf((Character) mapElement.getKey());
+        for (int i = 0; i < s.length(); i++) {
+            if (hashMap.get(s.charAt(i)) == 1) {
+                return i;
             }
         }
         return -1;
@@ -28,17 +28,27 @@ public class FirstUniqueCharacterInString {
     void firstTest() {
         int expected = 0;
         int actual = firstUniqChar("leetcode");
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void secondTest() {
         int expected = 2;
         int actual = firstUniqChar("loveleetcode");
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void lastTest() {
         int expected = -1;
         int actual = firstUniqChar("aabb");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void failTest() {
+        int expected = 0;
+        int actual = firstUniqChar("z");
+        Assertions.assertEquals(expected, actual);
     }
 }
