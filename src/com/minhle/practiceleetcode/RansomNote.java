b5a872d3034284c138a30d9c801024d1c402434a
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-10-12 02:09:31 -0400

    #383 RansomNote Using 2 hashmaps

--- src/com/minhle/practiceleetcode/RansomNote.java
+++ src/com/minhle/practiceleetcode/RansomNote.java
@@ -3,19 +3,42 @@ package com.minhle.practiceleetcode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 public class RansomNote {
+    private static HashMap<Character, Integer> getCharFrequency(String s) {
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (hashMap.containsKey(s.charAt(i))) {
+                hashMap.put(s.charAt(i), hashMap.get(s.charAt(i)) + 1);
+            }
+            else {
+                hashMap.put(s.charAt(i), 1);
+            }
+        }
+        return hashMap;
+    }
     public boolean canConstruct(String ransomNote, String magazine) {
         if (ransomNote.length() == 1 && magazine.length() == 1) {
             return ransomNote.equals(magazine);
         }
-        if (ransomNote.length() == 0) {
-            return true;
+        if (ransomNote.length() > magazine.length()) {
+            return false;
         }
-        if (magazine.indexOf(ransomNote.charAt(0)) >= 0) {
-            char c = ransomNote.charAt(0);
-            return canConstruct(ransomNote.replaceFirst(String.valueOf(c), ""), magazine.replaceFirst(String.valueOf(c), ""));
+        HashMap<Character, Integer> ransomNoteCount = getCharFrequency(ransomNote);
+        HashMap<Character, Integer> magazineCount = getCharFrequency(magazine);
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if (!(magazineCount.containsKey(ransomNote.charAt(i)))) {
+                return false;
+            }
+            else {
+                if (!(ransomNoteCount.get(ransomNote.charAt(i)) <= magazineCount.get(ransomNote.charAt(i)))) {
+                    return false;
+                }
+            }
         }
-        return false;
+        return true;
+
     }
 
     @Test
@@ -37,5 +60,12 @@ public class RansomNote {
         String ransomNote = "aa";
         String magazine = "aba";
         Assertions.assertTrue(canConstruct(ransomNote, magazine));
+    }
+
+    @Test
+    void failTest() {
+        String ransomNote = "az";
+        String magazine = "ab";
+        Assertions.assertFalse(canConstruct(ransomNote, magazine));
     }
 }
