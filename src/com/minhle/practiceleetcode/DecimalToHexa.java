f8767f69b4f5eb2b1cc57579ec63413c165c2337
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-10-20 02:59:19 -0400

    Changed back convert decimal to hexa

--- src/com/minhle/practiceleetcode/DecimalToHexa.java
+++ src/com/minhle/practiceleetcode/DecimalToHexa.java
@@ -6,11 +6,7 @@ import org.junit.jupiter.api.Test;
 public class DecimalToHexa {
     static void converter(int x) {
         int remainder = x % 16;
-        int xCopy = x;
-        int remainderCopy = xCopy % 16;
-        int r = remainderCopy;
         int ascii = 0;
-        int iterationCount = 0;
         while (remainder != 0) {
             x /= 16;
             if (remainder < 10) {
@@ -19,26 +15,7 @@ public class DecimalToHexa {
                 ascii = 55 + remainder;
             }
             remainder = x % 16;
-            iterationCount ++;
-        }
-        int printCount = 0;
-        while (printCount <= iterationCount) {
-            while (remainderCopy != 0) {
-                xCopy /= 16;
-                if (remainderCopy < 10) {
-                    ascii = 48 + remainderCopy;
-                } else {
-                    ascii = 55 + remainderCopy;
-                }
-                remainderCopy = xCopy % 16;
-                for (int count = iterationCount; count >= 0; count-- ) {
-                    if (count - printCount >= 0) {
-                        System.out.print((char) ascii);
-                        printCount++;
-                    }
-                }
-
-            }
+            System.out.println((char) ascii);
         }
 
 
