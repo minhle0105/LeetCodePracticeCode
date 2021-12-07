aecbba326f3c887db0feea81420fe7ec6cde2474
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-12-07 01:46:56 -0500

    160 IntersectionOfTwoLinkedList with test cases

--- src/com/minhle/practiceleetcode/ListNode.java
+++ src/com/minhle/practiceleetcode/ListNode.java
@@ -15,4 +15,23 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+
+    public boolean compareTo(ListNode o) {
+        if (o == null) {
+            return false;
+        }
+        if (this.val != o.val) {
+            return false;
+        }
+        ListNode cur1 = this;
+        ListNode cur2 = o;
+        while (cur1.next != null && cur2.next != null) {
+            if (cur1.val != cur2.val) {
+                return false;
+            }
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return !((cur1.next == null && cur2.next != null) | (cur1.next != null && cur2.next == null));
+    }
 }
