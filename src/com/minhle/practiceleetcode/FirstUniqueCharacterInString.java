98b213a6cc65bd224cbbe278331a25d41b2be858
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-01-16 22:33:28 -0500

    First Unique Character in string using sorting

--- src/com/minhle/practiceleetcode/FirstUniqueCharacterInString.java
+++ src/com/minhle/practiceleetcode/FirstUniqueCharacterInString.java
@@ -3,26 +3,69 @@ package com.minhle.practiceleetcode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
+import java.util.*;
+
+class MyCharacter implements Comparable<MyCharacter> {
+    char val;
+    int index;
+    int frequency;
+
+    public MyCharacter(char val, int index, int frequency) {
+        this.val = val;
+        this.index = index;
+        this.frequency = frequency;
+    }
+
+
+    @Override
+    public int compareTo(MyCharacter o) {
+        if (this.frequency == o.frequency) {
+            return Integer.compare(this.index, o.index);
+        }
+        return Integer.compare(this.frequency, o.frequency);
+    }
+}
 
 public class FirstUniqueCharacterInString {
+
     public int firstUniqChar(String s) {
-        HashMap<Character, Integer> hashMap = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (hashMap.containsKey(s.charAt(i))) {
-                hashMap.put(s.charAt(i), hashMap.get(s.charAt(i)) + 1);
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
             }
             else {
-                hashMap.put(s.charAt(i), 1);
+                map.put(c, 1);
             }
         }
+        List<MyCharacter> characters = new ArrayList<>();
         for (int i = 0; i < s.length(); i++) {
-            if (hashMap.get(s.charAt(i)) == 1) {
-                return i;
-            }
+            characters.add(new MyCharacter(s.charAt(i), i, map.get(s.charAt(i))));
+        }
+        Collections.sort(characters);
+        if (characters.get(0).frequency == 1) {
+            return characters.get(0).index;
         }
         return -1;
     }
+
+//    public int firstUniqChar(String s) {
+//        HashMap<Character, Integer> hashMap = new HashMap<>();
+//        for (int i = 0; i < s.length(); i++) {
+//            if (hashMap.containsKey(s.charAt(i))) {
+//                hashMap.put(s.charAt(i), hashMap.get(s.charAt(i)) + 1);
+//            }
+//            else {
+//                hashMap.put(s.charAt(i), 1);
+//            }
+//        }
+//        for (int i = 0; i < s.length(); i++) {
+//            if (hashMap.get(s.charAt(i)) == 1) {
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
 
     @Test
     void firstTest() {
