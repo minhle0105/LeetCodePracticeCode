1d0707a23dd53919e1ae5463da729ac4b40bc9c0
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-01-20 22:50:07 -0500

    #692 Top K Frequent Words

--- src/com/minhle/practiceleetcode/CoinChange.java
+++ src/com/minhle/practiceleetcode/CoinChange.java
@@ -1,2 +1,35 @@
-package com.minhle.practiceleetcode;public class CoinChange {
+package com.minhle.practiceleetcode;
+
+import java.util.Arrays;
+
+public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        return minChange(amount, coins);
+    }
+    private static int minChange(int a, int[] ds) {
+        if (ds.length == 0) {
+            return -1;
+        }
+        if (a == 0) {
+            return 0;
+        }
+        if (ds[0] > a) {
+            return minChange(a, Arrays.copyOfRange(ds, 1, ds.length));
+        }
+        return min(specialAdd(1, minChange(a - ds[0], ds)), minChange(a, Arrays.copyOfRange(ds,1 , ds.length)));
+    }
+
+    private static int specialAdd(int a, int b) {
+        if (b == -1) {
+            return -1;
+        }
+        return a + b;
+    }
+
+    private static int min(int a, int b) {
+        if (b == -1) {
+            return a;
+        }
+        return Math.min(a, b);
+    }
 }
