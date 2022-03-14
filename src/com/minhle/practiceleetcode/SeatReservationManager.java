9dcfe8ff307d5617d5396ba7f8dadc8c522fc550
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-03-14 18:55:45 -0400

    #1845 Seat Reservation Manager

--- src/com/minhle/practiceleetcode/SeatReservationManager.java
+++ src/com/minhle/practiceleetcode/SeatReservationManager.java
@@ -1,2 +1,30 @@
-package com.minhle.practiceleetcode;public class SeatReservationManager {
+package com.minhle.practiceleetcode;
+
+import java.util.PriorityQueue;
+
+public class SeatReservationManager {
+    private int[] seats;
+    private PriorityQueue<Integer> heap;
+
+    public SeatReservationManager(int n) {
+        heap = new PriorityQueue<>();
+        seats = new int[n + 1];
+        for (int i = 1; i < seats.length; i++) {
+            heap.add(i);
+        }
+    }
+
+    public int reserve() {
+        if (heap.isEmpty()) {
+            return -1;
+        }
+        int minSeat = heap.remove();
+        seats[minSeat] = 1;
+        return minSeat;
+    }
+
+    public void unreserve(int seatNumber) {
+        seats[seatNumber] = 0;
+        heap.add(seatNumber);
+    }
 }
