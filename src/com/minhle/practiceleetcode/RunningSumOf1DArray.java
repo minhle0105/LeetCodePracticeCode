b38258e39a3276eefb722971a072a4394fdde7fd
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-06-01 10:46:42 +0700

    #1480 Running Sum of 1D Array

--- src/com/minhle/practiceleetcode/RunningSumOf1DArray.java
+++ src/com/minhle/practiceleetcode/RunningSumOf1DArray.java
@@ -3,12 +3,9 @@ package com.minhle.practiceleetcode;
 public class RunningSumOf1DArray {
     public int[] runningSum(int[] nums) {
         int n = nums.length;
-        int[] runningSum = new int[n];
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            runningSum[i] = nums[i] + sum;
-            sum = runningSum[i];
+        for (int i = 1; i < n; i++) {
+            nums[i] += nums[i - 1];
         }
-        return runningSum;
+        return nums;
     }
 }
