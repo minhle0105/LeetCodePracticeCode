f43c62ca6abc7f9c4a15c942ef9a07bee272812b
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-06-18 15:00:29 +0700

    #380 Insert Delete GetRandom O(1)

--- src/com/minhle/practiceleetcode/InsertDeleteGetRandomO1.java
+++ src/com/minhle/practiceleetcode/InsertDeleteGetRandomO1.java
@@ -38,14 +38,4 @@ public class InsertDeleteGetRandomO1 {
             return list.get(rand.nextInt(list.size()));
         }
     }
-
-    public static void main(String[] args) {
-        RandomizedSet randomizedSet = new RandomizedSet();
-        System.out.println(randomizedSet.remove(0));
-        System.out.println(randomizedSet.remove(0));
-        System.out.println(randomizedSet.insert(0));
-        System.out.println(randomizedSet.getRandom());
-        System.out.println(randomizedSet.remove(0));
-        System.out.println(randomizedSet.insert(0));
-    }
 }
