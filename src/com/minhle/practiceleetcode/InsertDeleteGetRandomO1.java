534295058c5b3da8e3fc49b9cc18e822d71c6eb4
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-06-18 10:30:09 +0700

    #380 Insert Delete GetRandom O(1)

---
+++ src/com/minhle/practiceleetcode/InsertDeleteGetRandomO1.java
@@ -0,0 +1,51 @@
+package com.minhle.practiceleetcode;
+
+import java.util.*;
+
+public class InsertDeleteGetRandomO1 {
+    static class RandomizedSet {
+        Map<Integer, Integer> map;
+        List<Integer> list;
+        Random rand;
+
+        public RandomizedSet() {
+            map = new HashMap<>();
+            list = new ArrayList<>();
+            rand = new Random();
+        }
+
+        public boolean insert(int val) {
+            if (map.containsKey(val)) return false;
+            map.put(val, list.size());
+            list.add(val);
+            return true;
+        }
+
+        public boolean remove(int val) {
+            if (!map.containsKey(val)) {
+                return false;
+            }
+            int lastElement = list.get(list.size() - 1);
+            int indexOfRemovedElement = map.get(val);
+            list.set(indexOfRemovedElement, lastElement);
+            map.put(lastElement, indexOfRemovedElement);
+            list.remove(list.size() - 1);
+            map.remove(val);
+            return true;
+        }
+
+        public int getRandom() {
+            return list.get(rand.nextInt(list.size()));
+        }
+    }
+
+    public static void main(String[] args) {
+        RandomizedSet randomizedSet = new RandomizedSet();
+        System.out.println(randomizedSet.remove(0));
+        System.out.println(randomizedSet.remove(0));
+        System.out.println(randomizedSet.insert(0));
+        System.out.println(randomizedSet.getRandom());
+        System.out.println(randomizedSet.remove(0));
+        System.out.println(randomizedSet.insert(0));
+    }
+}
