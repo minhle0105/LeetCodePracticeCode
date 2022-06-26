945c1f73a6fbcc353aed9b0ac30b9af6ebbe9ad9
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-06-26 16:55:27 +0700

    #108 Convert Sorted Array To Binary Search Tree

---
+++ src/com/minhle/practiceleetcode/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,17 @@
+package com.minhle.practiceleetcode;
+
+public class ConvertSortedArrayToBinarySearchTree {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return insert(nums, 0, nums.length - 1);
+    }
+    private TreeNode insert(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = insert(nums, left, mid - 1);
+        root.right = insert(nums, mid + 1, right);
+        return root;
+    }
+}
