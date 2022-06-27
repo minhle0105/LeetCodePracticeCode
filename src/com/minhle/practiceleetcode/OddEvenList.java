92f350bfceeec29dc1aec90a79a2e874ccbf226d
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-06-27 15:55:56 +0700

    #328 Odd Even LinkedList

---
+++ src/com/minhle/practiceleetcode/OddEvenList.java
@@ -0,0 +1,37 @@
+package com.minhle.practiceleetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OddEvenList {
+    public ListNode oddEvenList(ListNode head) {
+        ListNode curr = head;
+
+        boolean isOdd = true;
+
+        ListNode odds = new ListNode();
+        ListNode currOdds = odds;
+
+        ListNode evens = new ListNode();
+        ListNode currEvens = evens;
+
+        while (curr != null) {
+            if (isOdd) {
+                currOdds.next = new ListNode(curr.val);
+                currOdds = currOdds.next;
+            }
+            else {
+                currEvens.next = new ListNode(curr.val);
+                currEvens = currEvens.next;
+            }
+            curr = curr.next;
+            isOdd = !isOdd;
+        }
+
+        odds = odds.next;
+        evens = evens.next;
+        currOdds.next = evens;
+
+        return odds;
+    }
+}
--- src/com/minhle/practiceleetcode/StringToInteger.java
+++ src/com/minhle/practiceleetcode/StringToInteger.java
@@ -6,50 +6,20 @@ import org.junit.jupiter.api.Test;
 public class StringToInteger {
     public int myAtoi(String s) {
         s = s.trim();
-        String[] strings = s.split(" ");
-        s = strings[0].trim();
-        StringBuilder str = new StringBuilder();
-        int firstInteger;
+        String[] words = s.split(" ");
+        for (String word : words) {
+            if (Character.isDigit(word.charAt(word.length() - 1))) {
+                return convert(word);
+            }
+        }
+        return -1;
+    }
 
+    private int convert(String s) {
         if (s.charAt(0) == '-') {
-            firstInteger = 1;
-            if (s.charAt(firstInteger) != '0') {
-                for (int i = 0; i < s.length(); i++) {
-                    str.append(s.charAt(i));
-                }
-            }
-            else {
-                for (int i = 1; i < s.length(); i++) {
-                    if (s.charAt(i) != '0') {
-                        firstInteger = i;
-                        break;
-                    }
-                }
-                for (int i = firstInteger; i < s.length(); i++) {
-                    str.append(s.charAt(i));
-                }
-            }
+            return Integer.parseInt(s.substring(1)) * -1;
         }
-        else {
-            firstInteger = 0;
-            if (s.charAt(firstInteger) != '0') {
-                for (int i = 0; i < s.length(); i++) {
-                    str.append(s.charAt(i));
-                }
-            }
-            else {
-                for (int i = 0; i < s.length(); i++) {
-                    if (s.charAt(i) != '0') {
-                        firstInteger = i;
-                        break;
-                    }
-                }
-                for (int i = firstInteger; i < s.length(); i++) {
-                    str.append(s.charAt(i));
-                }
-            }
-        }
-        return Integer.parseInt(str.toString());
+        return Integer.parseInt(s);
     }
 
     @Test
@@ -72,6 +42,13 @@ public class StringToInteger {
     void test3() {
         String s = "4193 with words";
         int expected = 4193;
+        int actual = myAtoi(s);
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void test4() {
+        String s = "words and 987";
+        int expected = 987;
         int actual = myAtoi(s);
         Assertions.assertEquals(expected, actual);
     }
