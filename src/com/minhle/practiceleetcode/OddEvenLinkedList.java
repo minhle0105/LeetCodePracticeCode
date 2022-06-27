b9a34fcdd6f7e5671bd5ab3c076b5151c7b594ec
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-06-27 15:56:35 +0700

    #328 Odd Even LinkedList

--- src/com/minhle/practiceleetcode/OddEvenLinkedList.java
+++ src/com/minhle/practiceleetcode/OddEvenLinkedList.java
@@ -1,9 +1,6 @@
 package com.minhle.practiceleetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class OddEvenList {
+public class OddEvenLinkedList {
     public ListNode oddEvenList(ListNode head) {
         ListNode curr = head;
 
