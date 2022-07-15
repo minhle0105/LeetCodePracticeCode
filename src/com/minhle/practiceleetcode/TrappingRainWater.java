8f0f01a6b7d120a40c4d01089ac271cfae7d8ce8
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-07-15 11:16:45 +0700

    #42 Trapping Rain Water

--- src/com/minhle/practiceleetcode/TrappingRainWater.java
+++ src/com/minhle/practiceleetcode/TrappingRainWater.java
@@ -5,25 +5,35 @@ import org.junit.jupiter.api.Test;
 public class TrappingRainWater {
     int[] maxLeft;
     int[] maxRight;
-
     int[] minLeftRight;
-    private void findMaxArrays(int[] input) {
+
+    private void calculateHelperArrays(int[] input) {
         maxLeft = new int[input.length];
         maxRight = new int[input.length];
         minLeftRight = new int[input.length];
+        findMaxFromLeft(input);
+        findMaxFromRight(input);
+        findMinLeftRight(input);
+    }
+    private void findMaxFromLeft(int[] input) {
         for (int i = 1; i < maxLeft.length; i++) {
             maxLeft[i] = Math.max(maxLeft[i - 1], input[i - 1]);
         }
+    }
+    private void findMaxFromRight(int[] input) {
         for (int i = maxRight.length - 2; i >= 0; i--) {
             maxRight[i] = Math.max(maxRight[i + 1], input[i + 1]);
         }
+    }
+
+    private void findMinLeftRight(int[] input) {
         for (int i = 0; i < minLeftRight.length; i++) {
             minLeftRight[i] = Math.min(maxLeft[i], maxRight[i]);
         }
     }
 
     private int calculateTotalTrappedWater(int[] input) {
-        findMaxArrays(input);
+        calculateHelperArrays(input);
         int[] totalTrappedWater = new int[input.length];
         for (int i = 0; i < totalTrappedWater.length; i++) {
             int water = minLeftRight[i] - input[i];
@@ -34,6 +44,10 @@ public class TrappingRainWater {
             sum += water;
         }
         return sum;
+    }
+
+    public int trap(int[] height) {
+        return calculateTotalTrappedWater(height);
     }
 
     @Test
