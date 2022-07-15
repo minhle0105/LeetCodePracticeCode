e0ad925daf5917f48691e8180e7c212d59423a21
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-07-15 11:56:17 +0700

    #657 Robot Return To Origin - Refactor

--- src/com/minhle/practiceleetcode/RobotReturnToOrigin.java
+++ src/com/minhle/practiceleetcode/RobotReturnToOrigin.java
@@ -1,25 +1,47 @@
 package com.minhle.practiceleetcode;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class RobotReturnToOrigin {
+    int[] currentPosition;
     public boolean judgeCircle(String moves) {
-        int[] currentPosition = {0,0};
+        currentPosition = new int[]{0,0};
         for (char c : moves.toCharArray()) {
             switch (c) {
                 case 'L':
-                    currentPosition[1]--;
+                    goLeft();
                     break;
                 case 'U':
-                    currentPosition[0]--;
+                    goUp();
                     break;
                 case 'D':
-                    currentPosition[0]++;
+                    goDown();
                     break;
                 case 'R':
-                    currentPosition[1]++;
+                    goRight();
                     break;
                 default:
             }
         }
         return currentPosition[0] == 0 && currentPosition[1] == 0;
+    }
+    private void goLeft() {
+        currentPosition[1]--;
+    }
+    private void goRight() {
+        currentPosition[1]++;
+    }
+    private void goUp() {
+        currentPosition[0]++;
+    }
+    private void goDown() {
+        currentPosition[0]--;
+    }
+
+    @Test
+    void test1() {
+        String moves = "UD";
+        Assertions.assertTrue(judgeCircle(moves));
     }
 }
