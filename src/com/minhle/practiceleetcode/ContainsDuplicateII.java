db9bb8d3ee14124476e8dacb0279d51e201475a3
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-07-21 15:31:27 +0700

    #219 Contains Duplicate II with test cases

--- src/com/minhle/practiceleetcode/ContainsDuplicateII.java
+++ src/com/minhle/practiceleetcode/ContainsDuplicateII.java
@@ -2,22 +2,39 @@ package com.minhle.practiceleetcode;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ContainsDuplicateII {
-    private boolean checkSubCondition(int i, int j, int k) {
-        return Math.abs(i-j) <= k;
-    }
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    if (checkSubCondition(i, j, k)) {
-                        return true;
-                    }
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.get(nums[i]).add(i);
+            }
+            else {
+                map.put(nums[i], new ArrayList<>());
+                map.get(nums[i]).add(i);
+            }
+        }
+        List<Integer> duplicateValues = new ArrayList<>();
+        for (Integer key : map.keySet()) {
+            if (map.get(key).size() > 1) {
+                duplicateValues.add(key);
+            }
+        }
+        if (duplicateValues.size() == 0) {
+            return false;
+        }
+        for (Integer value : duplicateValues) {
+            List<Integer> indices = map.get(value);
+            for (int i = 0; i < indices.size() - 1; i++) {
+                if (Math.abs(indices.get(i + 1) - indices.get(i)) <= k) {
+                    return true;
                 }
             }
         }
