068af4d43680c8eafac2082683433eec4d2bd68b
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-11-13 17:17:02 -0500

    #186 Reverse Words In A String II

---
+++ src/com/minhle/practiceleetcode/ReverseWordsInAStringII.java
@@ -0,0 +1,36 @@
+package com.minhle.practiceleetcode;
+
+import org.junit.jupiter.api.Test;
+
+public class ReverseWordsInAStringII {
+    public static void reverseWords(char[] s) {
+        for (int i = 0; i < s.length / 2; i++) {
+            char temp = s[i];
+            s[i] = s[s.length - i - 1];
+            s[s.length - i - 1] = temp;
+        }
+        int i = 0;
+        int j = i;
+        while (i < s.length) {
+            while (i < s.length && s[i] != ' ') {
+                i++;
+            }
+            i--;
+            int k = i;
+            while (j < k) {
+                char temp = s[j];
+                s[j] = s[k];
+                s[k] = temp;
+                j++;
+                k--;
+            }
+            i+=2;
+            j = i;
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] s = "the sky is blue".toCharArray();
+        reverseWords(s);
+    }
+}
