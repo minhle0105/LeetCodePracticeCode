e307d5ad89e28829b36d0bcd0848b1808f65e87d
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-11-18 10:48:29 -0500

    #222 Count Number Of Tree Nodes

---
+++ src/com/minhle/practiceleetcode/CountNumberOfTreeNodes.java
@@ -0,0 +1,10 @@
+package com.minhle.practiceleetcode;
+
+public class CountNumberOfTreeNodes {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
