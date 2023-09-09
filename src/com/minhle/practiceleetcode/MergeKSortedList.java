1ca05cf41f0b9284791285085d70fd5f9091b817
minhle0105 <minh.anh.le.cnn@gmail.com>
2023-09-08 22:47:20 -0400

    #23 Merge K Sorted List modify

--- src/com/minhle/practiceleetcode/MergeKSortedList.java
+++ src/com/minhle/practiceleetcode/MergeKSortedList.java
@@ -2,23 +2,35 @@ package com.minhle.practiceleetcode;
 
 import java.util.*;
 
-public class MergeKSortedList {
+public class MergeKSortedList
+{
+    static class ListNode
+    {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
     public ListNode mergeKLists(ListNode[] lists) {
-        List<Integer> nodeToStr = new ArrayList<>();
-        for (ListNode node : lists) {
-            ListNode currNode = node;
-            while (currNode != null) {
-                nodeToStr.add(currNode.val);
-                currNode = currNode.next;
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for (ListNode node : lists)
+        {
+            ListNode curr = node;
+            while (curr != null)
+            {
+                heap.add(curr.val);
+                curr = curr.next;
             }
         }
-        PriorityQueue<Integer> heap = new PriorityQueue<>(nodeToStr);
-        ListNode result = new ListNode();
-        ListNode curr = result;
-        while (!heap.isEmpty()) {
+        ListNode dummy_head = new ListNode(-1);
+        ListNode curr = dummy_head;
+        while (!heap.isEmpty())
+        {
             curr.next = new ListNode(heap.remove());
             curr = curr.next;
         }
-        return result.next;
+        return dummy_head.next;
     }
 }
