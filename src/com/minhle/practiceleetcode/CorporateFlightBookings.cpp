d126777cd84e987ca4cc33649fd8d62df92fd801
minhle0105 <minh.anh.le.cnn@gmail.com>
2023-09-20 17:41:01 -0400

    #1109 Corporate Flight Bookings

--- src/com/minhle/practiceleetcode/CorporateFlightBookings.cpp
+++ src/com/minhle/practiceleetcode/CorporateFlightBookings.cpp
@@ -5,14 +5,21 @@ using namespace std;
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-        vector<int> res(n, 0);
+        vector<int> diff(n + 2, 0);
         for (const auto &booking : bookings)
         {
-            for (int i = booking.at(0); i <= booking.at(1); ++i)
-            {
-                res.at(i - 1) += booking.at(2);
-            }
+            int start = booking.at(0);
+            int end = booking.at(1);
+            int amount = booking.at(2);
+            diff.at(start) += amount;
+            diff.at(end + 1) -= amount;
         }
-        return res;
+        for (int i = 0; i < n; ++i)
+        {
+            diff.at(i + 1) = diff.at(i + 1) + diff.at(i);
+        }
+        diff.erase(diff.begin());
+        diff.pop_back();
+        return diff;
     }
 };
