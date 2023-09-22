2a213380ae5751cc116c6428e0718e386131acbb
minhle0105 <minh.anh.le.cnn@gmail.com>
2023-09-21 23:52:02 -0400

    #718 Subarray Product Less Than K

---
+++ src/com/minhle/practiceleetcode/SubarrayProductLessThanK.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int res = 0;
+        if (k <= 1)
+        {
+            return 0;
+        }
+        int first = 0;
+        int second = 0;
+        int current = 1;
+        while (first < nums.size())
+        {
+            current *= nums.at(first);
+            while (second < nums.size() && current >= k)
+            {
+                current /= nums.at(second);
+                ++second;
+            }
+            res += (first - second + 1);
+            ++first;
+        }
+        return res;
+    }
+};
