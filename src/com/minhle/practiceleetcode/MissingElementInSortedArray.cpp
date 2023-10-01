705cb6bd423d9d028859d8e611beeb392d2074a8
minhle0105 <minh.anh.le.cnn@gmail.com>
2023-10-01 14:54:21 -0400

    #1060 Missing Element in Sorted Array - O(log(N)) solution

--- src/com/minhle/practiceleetcode/MissingElementInSortedArray.cpp
+++ src/com/minhle/practiceleetcode/MissingElementInSortedArray.cpp
@@ -5,23 +5,13 @@ using namespace std;
 class Solution {
 public:
     int missingElement(vector<int>& nums, int k) {
-        vector<int> prefix_missing;
-        prefix_missing.reserve(nums.size());
-        prefix_missing.push_back(0);
-        for (int i = 1; i < nums.size(); ++i)
-        {
-            prefix_missing.push_back(prefix_missing.back() + (nums.at(i) - nums.at(i - 1) - 1));
-            if (prefix_missing.back() > k)
-            {
-                break;
-            }
-        }
         int left = 0;
-        int right = prefix_missing.size() - 1;
+        int right = nums.size() - 1;
         while (left < right - 1)
         {
             int mid = (left + right) / 2;
-            if (prefix_missing.at(mid) < k)
+            int nMissingAtMid = findNMissingAtNum(nums.at(mid), nums.front(), mid);
+            if (nMissingAtMid < k)
             {
                 left = mid;
             }
@@ -30,10 +20,16 @@ public:
                 right = mid - 1;
             }
         }
-        if (prefix_missing.at(right) < k)
+        int nMissingAtRight = findNMissingAtNum(nums.at(right), nums.front(), right);
+        if (nMissingAtRight < k)
         {
-            return nums.at(right) + (k - prefix_missing.at(right));
+            return nums.at(right) + (k - nMissingAtRight);
         }
-        return nums.at(left) + (k - prefix_missing.at(left));
+        return nums.at(left) + (k - findNMissingAtNum(nums.at(left), nums.front(), left));
+    }
+
+    int findNMissingAtNum(int num, const int beginning, int i)
+    {
+        return num - beginning - i;
     }
 };
