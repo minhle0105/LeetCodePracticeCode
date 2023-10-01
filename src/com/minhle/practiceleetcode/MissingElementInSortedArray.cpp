9104aa35de257af2c58dde75d168fc8a4febec6c
minhle0105 <minh.anh.le.cnn@gmail.com>
2023-10-01 14:16:56 -0400

    #1060 Missing Element in Sorted Array - Make some improvements in the code

--- src/com/minhle/practiceleetcode/MissingElementInSortedArray.cpp
+++ src/com/minhle/practiceleetcode/MissingElementInSortedArray.cpp
@@ -6,10 +6,15 @@ class Solution {
 public:
     int missingElement(vector<int>& nums, int k) {
         vector<int> prefix_missing;
+        prefix_missing.reserve(nums.size());
         prefix_missing.push_back(0);
         for (int i = 1; i < nums.size(); ++i)
         {
             prefix_missing.push_back(prefix_missing.back() + (nums.at(i) - nums.at(i - 1) - 1));
+            if (prefix_missing.back() > k)
+            {
+                break;
+            }
         }
         int left = 0;
         int right = prefix_missing.size() - 1;
