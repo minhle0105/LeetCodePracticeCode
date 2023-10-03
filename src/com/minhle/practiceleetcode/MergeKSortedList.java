f2bcb343812d354e2ea88104db2060ba8ade5633
minhle0105 <minh.anh.le.cnn@gmail.com>
2023-10-03 13:40:48 -0400

    #23 Merge K Sorted List modify - Remove usage of built-in data structure

--- src/com/minhle/practiceleetcode/MergeKSortedList.java
+++ src/com/minhle/practiceleetcode/MergeKSortedList.java
@@ -14,23 +14,57 @@ public class MergeKSortedList
     }
 
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
-        for (ListNode node : lists)
+        if (lists.length == 0)
         {
-            ListNode curr = node;
-            while (curr != null)
+            return null;
+        }
+        if (lists.length == 1)
+        {
+            return lists[0];
+        }
+        ListNode res = mergeTwoList(lists[0], lists[1]);
+        for (int i = 2; i < lists.length; ++i)
+        {
+            res = mergeTwoList(res, lists[i]);
+        }
+        return res;
+    }
+
+    private ListNode mergeTwoList(ListNode node1, ListNode node2)
+    {
+        ListNode curr1 = node1;
+        ListNode curr2 = node2;
+        ListNode res = new ListNode(-1);
+        ListNode curr = res;
+        while (curr1 != null && curr2 != null)
+        {
+            if (curr1.val < curr2.val)
             {
-                heap.add(curr.val);
+                ListNode next = new ListNode(curr1.val);
+                curr.next = next;
                 curr = curr.next;
+                curr1 = curr1.next;
+            }
+            else
+            {
+                ListNode next = new ListNode(curr2.val);
+                curr.next = next;
+                curr = curr.next;
+                curr2 = curr2.next;
             }
         }
-        ListNode dummy_head = new ListNode(-1);
-        ListNode curr = dummy_head;
-        while (!heap.isEmpty())
+        while (curr1 != null)
         {
-            curr.next = new ListNode(heap.remove());
+            curr.next = new ListNode(curr1.val);
             curr = curr.next;
+            curr1 = curr1.next;
         }
-        return dummy_head.next;
+        while (curr2 != null)
+        {
+            curr.next = new ListNode(curr2.val);
+            curr = curr.next;
+            curr2 = curr2.next;
+        }
+        return res.next;
     }
 }
