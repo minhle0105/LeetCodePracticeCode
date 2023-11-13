a7533a8a39c3d06f0179fdc3aafcb11d44e80023
minhle0105 <minh.anh.le.cnn@gmail.com>
2023-11-13 18:03:44 -0500

    Create a file to keep all the necessary include for a standard question to avoid manually include everything each time

---
+++ src/com/minhle/practiceleetcode/AllNecessaryHeaders.h
@@ -0,0 +1,18 @@
+#ifndef allnecessaryheaders
+#define allnecessaryheaders
+
+#include <algorithm>
+#include <functional>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+#endif
