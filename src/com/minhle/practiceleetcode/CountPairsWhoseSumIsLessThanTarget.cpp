78baa806d156d63c551120b9a1f1f14e24e977ae
minhle0105 <minh.anh.le.cnn@gmail.com>
2023-11-17 12:17:25 -0500

    #2824 Count Pairs Whose Sum Is Less Than Target

---
+++ src/com/minhle/practiceleetcode/CountPairsWhoseSumIsLessThanTarget.cpp
@@ -0,0 +1,19 @@
+#include "AllNecessaryHeaders.h"
+
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int res = 0;
+        for (int i = 0; i < nums.size() - 1; ++i)
+        {
+            for (int j = i + 1; j < nums.size(); ++j)
+            {
+                if (nums[i] + nums[j] < target)
+                {
+                    ++res;
+                }
+            }
+        }   
+        return res;
+    }
+};
