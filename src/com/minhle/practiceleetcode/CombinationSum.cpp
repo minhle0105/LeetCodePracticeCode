ea76ecb0a09cdaf0c7c9b2abd64480ff1347c2ad
minhle0105 <minh.anh.le.cnn@gmail.com>
2023-11-30 10:36:51 -0500

    #39 Combination Sum

--- src/com/minhle/practiceleetcode/CombinationSum.cpp
+++ src/com/minhle/practiceleetcode/CombinationSum.cpp
@@ -1,4 +1,3 @@
-#include <iterator>
 #include <AllNecessaryHeaders.h>
 
 class Solution {
@@ -7,47 +6,28 @@ public:
     {
         vector<vector<int>> res;
         vector<int> combination;
-        set<vector<int>> sorted_combinations;
-        backtracking(candidates, combination, 0, target, res, sorted_combinations);
+        backtracking(candidates, combination, 0, target, 0, res);
         return res;
     }
 private:
     void backtracking(const vector<int> &candidates, vector<int> &combination,
-                      int current_sum, int target,
-                      vector<vector<int>> &res, set<vector<int>> &sorted_combinations)
+                      int current_sum, int target, int pos,
+                      vector<vector<int>> &res)
     {
         if (current_sum == target)
         {
-            if (combination_is_unique(combination, sorted_combinations))
-            {
-                res.push_back(combination);
-            }
+            res.push_back(combination);
             return;
         }
-        if (current_sum > target)
+        if (pos >= candidates.size() || current_sum > target)
         {
             return;
         }
-        for (int i = 0; i < candidates.size(); ++i)
-        {
-            current_sum += candidates[i];
-            combination.push_back(candidates[i]);
-            backtracking(candidates, combination, current_sum, target, res, sorted_combinations);
-            combination.pop_back();
-            current_sum -= candidates[i];
-        }
-    }
-
-    bool combination_is_unique(const vector<int> &combination, set<vector<int>> &sorted_combinations)
-    {
-        vector<int> copy;
-        std::copy(combination.begin(), combination.end(), back_inserter(copy));
-        sort(copy.begin(), copy.end());
-        if (sorted_combinations.find(copy) == sorted_combinations.end())
-        {
-            sorted_combinations.insert(copy);
-            return true;
-        }
-        return false;
+        current_sum += candidates[pos];
+        combination.push_back(candidates[pos]);
+        backtracking(candidates, combination, current_sum, target, pos, res);
+        combination.pop_back();
+        current_sum -= candidates[pos];
+        backtracking(candidates, combination, current_sum, target, pos + 1, res);
     }
 };
