5aabec2367b6bc9aab59320dfd270e12e7fcbc00
minhle0105 <minh.anh.le.cnn@gmail.com>
2023-12-11 04:39:41 -0500

    #79 Word Search

--- src/com/minhle/practiceleetcode/WordSearch.cpp
+++ src/com/minhle/practiceleetcode/WordSearch.cpp
@@ -3,21 +3,13 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        string curr;
         for (int i = 0; i < board.size(); ++i)
         {
             for (int j = 0; j < board[i].size(); ++j)
             {
-                if (board[i][j] == word.front())
+                if (backtracking(board, word, i, j, 0))
                 {
-                    curr += word.front();
-                    vector<vector<bool>> visited(board.size(), vector<bool>(board[0].size(), false));
-                    visited[i][j] = true;
-                    if (backtracking(board, word, curr, 1, i, j, visited))
-                    {
-                        return true;
-                    }
-                    curr.pop_back();
+                    return true;
                 }
             }
         }
@@ -27,42 +19,34 @@ private:
     const vector<int> dR = {0, 0, 1, -1};
     const vector<int> dC = {1, -1, 0, 0};
 
-    bool backtracking(const vector<vector<char>> &board, const string &word, string &curr, int pos, int r, int c, vector<vector<bool>> &visited)
+    bool backtracking(vector<vector<char>> &board, const string &word, int r, int c, int pos)
     {
-        if (curr.size() == word.size())
+        if (pos == word.size())
         {
             return true;
         }
-        if (r == board.size())
+        if (r < 0 || c < 0 || r >= board.size() || c >= board[0].size())
         {
             return false;
         }
-        if (c == board[0].size())
+        if (word[pos] != board[r][c])
         {
-            return backtracking(board, word, curr, pos, r + 1, c, visited);
+            return false;
         }
-        for (int i = 0; i < 4; ++i)
-        {
-            int next_r = r + dR[i];
-            int next_c = c + dC[i];
-            bool in_bound = next_r >= 0 && next_c >= 0 && next_r < board.size() && next_c < board[0].size();
-            if (in_bound)
+
+        char temp = board[r][c];
+        board[r][c] = '#';
+
+        for (int i = 0; i < 4; ++i) {
+            int nextR = r + dR[i];
+            int nextC = c + dC[i];
+            if (backtracking(board, word, nextR, nextC, pos + 1))
             {
-                if (!visited[next_r][next_c] && board[next_r][next_c] == word[pos])
-                {
-                    curr += board[next_r][next_c];
-                    visited[next_r][next_c] = true;
-                    ++pos;
-                    if (backtracking(board, word, curr, pos, next_r, next_c, visited))
-                    {
-                        return true;
-                    }
-                    --pos;
-                    visited[next_r][next_c] = false;
-                    curr.pop_back();
-                }
+                return true;
             }
         }
+
+        board[r][c] = temp;
         return false;
     }
 };
