9b23bc79c10e60735671fd0493eb8e2065748d2d
minhle0105 <minh.anh.le.cnn@gmail.com>
2024-01-29 08:17:39 -0500

    Commit

--- src/com/minhle/practiceleetcode/StoneGame.cpp
+++ src/com/minhle/practiceleetcode/StoneGame.cpp
@@ -4,10 +4,10 @@ class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
         vector<vector<int>> dp(piles.size(), vector<int>(piles.size(), 0));
-        return play(piles, 0, piles.size() - 1, true, dp) > 0;
+        return play(piles, 0, piles.size() - 1, 1, dp) > 0;
     }
 
-    int play(const vector<int> &piles, int i, int j, bool alice, vector<vector<int>> &dp)
+    int play(const vector<int> &piles, int i, int j, int alice, vector<vector<int>> &dp)
     {
         if (i > j)
         {
@@ -17,18 +17,8 @@ public:
         {
             return dp[i][j];
         }
-        pair<int, int> cases;
-        if (alice)
-        {
-            cases.first = piles[i] + play(piles, i + 1, j, false, dp);
-            cases.second = piles[j] + play(piles, i, j - 1, false, dp);
-        }
-        else
-        {
-            cases.first = piles[i] * (-1) + play(piles, i + 1, j, true, dp);
-            cases.second = piles[j] * (-1) + play(piles, i, j - 1, true, dp);
-        }
-        dp[i][j] = max(cases.first, cases.second);
+        dp[i][j] = max(piles[i] * alice + play(piles, i + 1, j, alice * (-1), dp),
+                       piles[j] * alice + play(piles, i, j - 1, alice * (-1), dp));
         return dp[i][j];
     }
 };
