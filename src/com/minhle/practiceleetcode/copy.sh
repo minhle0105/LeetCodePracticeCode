18bda29cfbd3d5766f79f54e9c11550cb21253ec
minhle0105 <minh.anh.le.cnn@gmail.com>
2024-02-20 17:40:09 -0500

    Change gitignore

--- src/com/minhle/practiceleetcode/copy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./GenerateFileName > output.txt
-
-# Copy the contents of the temporary file to the clipboard
-cat output.txt | pbcopy
-
-# Clean up temporary files
-rm output.txt
--- src/com/minhle/practiceleetcode/git.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git add .
-git commit -m "Test commit bash"
-git push origin main
